15d077be9b6b6856179357bfaca18908ed1c984c
Aiden Tepper <aidenjtep@gmail.com>
2022-10-08 15:59:34 -0500

    refactor, working system commands

--- p2a/wish.c
+++ p2a/wish.c
@@ -2,56 +2,68 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
-
-void execute(char *argv[]);
-
-int main(int argc, char *argv[]) {
-    while(1) {
-        char *buffer;
-        size_t bufsize = 32;
-        size_t characters;
-        buffer = (char *)malloc(bufsize * sizeof(char));
-
-        printf("wish> ");
-        characters = getline(&buffer, &bufsize, stdin);
-
-        if(characters == -1 || strcmp(buffer, "exit\n") == 0) {
-            exit(0);
-        }
-
-        if(characters != -1) {
-            buffer[characters-1] = '\0';
-
-            char *tokens[32];
-            char *token;
-            token = strtok(buffer, " ");
-            for(int i = 0; token != NULL; i++) {
-                tokens[i] = token;
-                token = strtok(NULL, " ");
-            }
-
-            execute(tokens);
-        }
-    }
-}
+#include <sys/wait.h>
 
 void execute(char *argv[]) {
-    char *tmp = strdup(argv[0]);
-    strcpy(argv[0], "/usr/bin/");
-    strcat(argv[0], tmp);
-
-    // char *input[2];
-    // input[0] = malloc(512);
-    // strcpy(input[0], path);
-    // input[1] = NULL;
+    char *path = malloc(5 + sizeof(char*));
+    strcpy(path, "/bin/");
+    strcat(path, argv[0]);
 
     pid_t pid = fork();
 
     if(pid == 0) {
-        int ret = execv(argv[0], argv);
+        int ret = execv(path, argv);
         printf("Fails to execute %s\n", argv[0]);
         exit(1);
     }
 
-    free(tmp);
+    int status;
+    waitpid(pid, &status, 0);
+
+    free(path);
+}
+
+char **parse_input(char *input) {
+    char **tokens = malloc(256 * sizeof(char*));
+    char *token;
+    int index = 0;
+
+    token = strtok(input, " ");
+    while(token != NULL) {
+        tokens[index++] = token;
+        token = strtok(NULL, " \n");
+    }
+
+    tokens[index] = NULL;
+    return tokens;
+}
+
+char *read_input() {
+    char *buffer;
+    size_t bufsize = 256;
+    size_t characters;
+    buffer = (char *)malloc(bufsize * sizeof(char));
+    characters = getline(&buffer, &bufsize, stdin);
+
+    if(characters == -1 || strcmp(buffer, "exit\n") == 0) {
+        exit(0);
+    }
+
+    return buffer;
+}
+
+int main(int argc, char *argv[]) {
+
+    char *input;
+    char **args;
+
+    while(1) {
+        printf("wish> ");
+        input = read_input();
+        args = parse_input(input);
+        execute(args);
+
+        free(input);
+        free(args);
+    }
 }
